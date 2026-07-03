90bc38427e8c13cf9079d12a425794389529d450
Priyanshu Upadhyay <priyanshuu839@gmail.com>
2026-07-03 22:51:26 +0530

    new migration added

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,10 +1,18 @@
 from app import db
 
 class url(db.Model):
-    __tablename__='url' 
-    id = db.Column(db.Integer,primary_key=True)
-    long_url=db.Column(db.Text,nullable=False)
-    short_url=db.Column(db.String(10),nullable=False,unique=True)
-    created_at=db.Column(db.DateTime(timezone=True),nullable=True,server_default=db.func.now())
-    expires_at=db.Column(db.DateTime(timezone = True))
-    
+    __tablename__ = "url"
+
+    id = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.Text, nullable=False)
+    short_url = db.Column(db.String(50), nullable=False, unique=True, index=True)
+    created_at = db.Column(
+        db.DateTime(timezone=True),
+        nullable=False,
+        server_default=db.func.now()
+    )
+    expires_at = db.Column(
+        db.DateTime(timezone=True),
+        nullable=False,
+        index=True
+    )
