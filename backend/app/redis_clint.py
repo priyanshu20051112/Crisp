9739ce2d34d0ad2e62e1eb444e9dd06b6dc9ac2f
Priyanshu Upadhyay <priyanshuu839@gmail.com>
2026-07-04 14:09:22 +0530

    fixed redis

--- backend/app/redis_clint.py
+++ backend/app/redis_clint.py
@@ -1,7 +1,18 @@
+# import redis
+# import os
+# r=redis.Redis(host=os.getenv('REDIS_HOST'),port=6379,decode_responses=True)
+
+# def redis_counter():
+#     counter = r.incr('url_shortner:counter')
+#     return counter
+
 import redis
 import os
-r=redis.Redis(host=os.getenv('REDIS_HOST'),port=6379,decode_responses=True)
+
+r = redis.Redis.from_url(
+    os.getenv("REDIS_URL"),
+    decode_responses=True
+)
 
 def redis_counter():
-    counter = r.incr('url_shortner:counter')
-    return counter
+    return r.incr("url_shortner:counter")
